cc15f7650328375047c8ec1b49da8e6cc45f4be4
bydmm <miku@maimoe.com>
2019-08-25 13:31:58 +0800

    添加默认跨域

--- middleware/cors.go
+++ middleware/cors.go
@@ -10,7 +10,7 @@ func Cors() gin.HandlerFunc {
 	config := cors.DefaultConfig()
 	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"}
 	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Cookie"}
-	config.AllowOrigins = []string{"http://www.example.com"}
+	config.AllowOrigins = []string{"http://www.example.com", "http://localhost:8080"}
 	config.AllowCredentials = true
 	return cors.New(config)
 }
