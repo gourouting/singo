134e63c5879096c53be347641923cb6da52d38fc
chengka <miku@maimoe.com>
2020-03-09 13:13:56 +0800

    add config to redis for bad network

--- cache/main.go
+++ cache/main.go
@@ -15,9 +15,10 @@ var RedisClient *redis.Client
 func Redis() {
 	db, _ := strconv.ParseUint(os.Getenv("REDIS_DB"), 10, 64)
 	client := redis.NewClient(&redis.Options{
-		Addr:     os.Getenv("REDIS_ADDR"),
-		Password: os.Getenv("REDIS_PW"),
-		DB:       int(db),
+		Addr:       os.Getenv("REDIS_ADDR"),
+		Password:   os.Getenv("REDIS_PW"),
+		DB:         int(db),
+		MaxRetries: 1,
 	})
 
 	_, err := client.Ping().Result()
